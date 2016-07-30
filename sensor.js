e4d7849d25bbad1a68663a1d16c5262e66081edc
boazblake <boazblake@gmail.com>
2016-07-30 12:04:59 -0500

    sensor is now adjusted w rotatary

--- sensor.js
+++ sensor.js
@@ -4,9 +4,19 @@ const Tessel = require("tessel-io");
 let board = new five.Board( { io: new Tessel() }  );
 
 board.on("ready", function() {
-  let rotary = new five.Sensor("a4");
+  let led = new five.Led("a5");
 
-  rotary.on("change", function() {
-    console.log("sensor changed!!", this.value);
+  let rotary = new five.Sensor({
+    pin: "a4",
+    threshold:5,
+    frequency:100
+  });
+
+  rotary.scale(0, 1024).on("change", function() {
+      // Analog sensors produce a 10-bit value,
+        // but led brightness is an 8-bit PWM value.
+        // Convert by shifting the value's bits
+        // two places to the right.
+        led.brightness(this.value >> 2)
   });
 });
